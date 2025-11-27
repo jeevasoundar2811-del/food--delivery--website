7899a675776dc3b1a8fb6123662dd4bfbffd80fc
Sanjeevrr <rajusanjeev79@gmail.com>
2025-11-27 14:41:28 +0530

    change in orderpage

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import Navbar from "./component/Navbar";
-import{FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+
+import Delivery from "./component/Delivery";
+import Order from "./component/Order";
 
 function App() {
   return (
     <>
       <Navbar/>
+      <Order/>
+    <Delivery/>
+    
     </>
   );
 }
