947e8eeed5a0c587c3ec8650cde5c03d0d76cae4
Sanjeevrr <rajusanjeev79@gmail.com>
2025-12-02 11:39:40 +0530

    Local updates to App and Order components

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,25 @@ import Navbar from "./component/Navbar";
 
 import Delivery from "./component/Delivery";
 import Order from "./component/Order";
+import Product from "./component/Product";
+import Menu from "./component/Menu";
+//import Order from "./component/Order.jsx";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Navbar />
+        <Product/>
+        <Menu/>
+
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/" element={<Order />} />
-          <Route path="/" element={<Delivery />} />
-          {/* <Route path="/" element={<Home />} /> */}
+          <Route path="/Order" element={<Order />} />
+          <Route path="/Delivery" element={<Delivery />} />
+          <Route path="/Menu" element={<Menu />} />
+
+          
         </Routes>
       </BrowserRouter>
     </>
